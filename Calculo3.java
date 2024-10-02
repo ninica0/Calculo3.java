516e24415a805fc52ed481db98d8c2deea506ba2
ninica0 <santosnico048@gmail.com>
2024-10-02 15:55:20 -0300

    feat: Sistema de cálculo 3

---
+++ Calculo3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Calculo3 {
+	public static void main(String[] args) {
+
+int A, B, C, D;
+int res;
+A = 2;
+B= 3;
+C = 5;
+D = 9;
+if (A==2||B<7) {
+	res = (A+2)*(B-2);
+}else {
+	res=(A+B)/D*(C+D);
+}
+System.out.println("O resultado é: " + res);
+
+    }
+}
